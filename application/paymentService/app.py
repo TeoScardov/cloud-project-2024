a4f32b1567bf5ea4a79d57db3c06b757b4e5f77f
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2024-04-16 14:23:51 +0200

    Delete application directory

--- application/paymentService/app.py
+++
@@ -1,6 +0,0 @@
-from paymentApp import app
-
-if __name__ == '__main__':
-    app.run(port=8000, debug=True)
-
-
--- application/purchaseService/app.py
+++
@@ -1,4 +0,0 @@
-from purchaseApp import app
-
-if __name__ == '__main__':
-    app.run(port=8000, debug=True)
