6c3ec60d347c4fe8da93cb725c290947f6000433
zamb0 <zamboni.fabio23@gmail.com>
2024-04-18 13:40:30 +0200

    deleted old utils, create toFlaskResponce

--- docker/purchase_service/purchaseApp/utils.py
+++ docker/purchase_service/purchaseApp/utils.py
@@ -1,25 +1,9 @@
-import requests
+from requests.models import Response as requestsResponse
+from flask.wrappers import Response as flaskResponse
+from flask import jsonify, make_response
 
-def get_username(request):
-    if isAuthenticated(request):
+def toFlaskResponse(response: requestsResponse)->flaskResponse:
 
-        user_token = request.authorization.__str__()[7:-1]
+    flask_response = make_response(jsonify(response.json()), response.status_code, response.headers.items())
 
-        request_url = 'http://127.0.0.1:8000/api/account/authenticate'
-        request_headers = {'Authorization': 'Bearer ' + user_token}
-        response = requests.post(request_url, headers=request_headers)
-
-        return response.json()['username']
-    else:
-        return None
-
-def isAuthenticated(request):
-    auth = request.authorization.__str__()[0:6]
-    print(auth)
-    if auth is None or auth != 'Bearer':
-        return False
-    else:
-        return True
-    
-def get_cart(username):
-    ...
+    return flask_response
