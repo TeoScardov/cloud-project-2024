ec67c168a1503f052abaa502a245707c967b4d92
Marija <marija.maneva01@universitadipavia.it>
2024-05-04 09:06:40 +0200

    price added

--- docker/product_catalog/app.py
+++ docker/product_catalog/app.py
@@ -6,7 +6,7 @@ from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@flask_db:5432/postgres'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://onlineshop:cloud2024@localhost/flask_db'
 
 db = SQLAlchemy(app)
 class Book(db.Model):
@@ -17,24 +17,32 @@ class Book(db.Model):
     author = db.Column(db.String(80), unique=True, nullable=False)
     publisher = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.String(120), unique=True, nullable=False)
+    price = db.Column(db.REAL)  # New column for price
 
     def json(self):
-        return {'id': self.id, 'title': self.title, 'author': self.author, 'publisher': self.publisher, 'description': self.description}
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'publisher': self.publisher,
+            'description': self.description,
+            'price': self.price
+        }
 
 with app.app_context():
     db.create_all()
 
 # test route
-@app.route('/test', methods = ['GET'])
+@app.route('/api/product/test', methods = ['GET'])
 def test():
     return make_response(jsonify({'message': 'test route'}), 200)
 
 # create a book
-@app.route('/books', methods=['POST'])
+@app.route('/api/product/create-book', methods=['POST'])
 def create_book():
     try:
         data = request.get_json()
-        new_book = Book(title =data['title'], author = data['author'], publisher = data['publisher'], description =data['description'])
+        new_book = Book(title =data['title'], author = data['author'], publisher = data['publisher'], description =data['description'], price = data['price'])
         db.session.add(new_book)
         db.session.commit()
         return make_response(jsonify({'message':'book created'}), 201)
@@ -43,7 +51,7 @@ def create_book():
 
 
 # get all books
-@app.route('/books', methods=['GET'])
+@app.route('/api/product/get-books', methods=['GET'])
 def get_books():
     try:
         books = Book.query.all()
@@ -51,19 +59,26 @@ def get_books():
     except Exception as e:
         return make_response(jsonify({'message': 'error getting books'}), 500)
 
+
 # get a book by id
-@app.route('/books/<int:id>', methods=['GET'])
-def get_book(id):
+@app.route('/api/product/get-book-details', methods=['POST'])
+def get_book_by_id():
     try:
-        book = Book.query.filter_by(id = id).first()
+        data = request.get_json()
+        book_id = data.get('id')
+        if book_id is None:
+            return make_response(jsonify({'message': 'ID not provided in request body'}), 400)
+
+        book = Book.query.get(book_id)
         if book:
             return make_response(jsonify({'book': book.json()}), 200)
         return make_response(jsonify({'message': 'book not found'}), 404)
     except Exception as e:
-        return make_response(jsonify({'message': 'error getting book'}),500)
+        return make_response(jsonify({'message': 'error getting book'}), 500)
+
 
 # update a book
-@app.route('/books/<int:id>', methods = ['PUT'])
+@app.route('/api/product/update-book/<int:id>', methods = ['PUT'])
 def update_book(id):
     try:
         book = Book.query.filter_by(id=id).first()
@@ -73,6 +88,7 @@ def update_book(id):
             book.author = data['author']
             book.publisher = data['publisher']
             book.description = data['description']
+            book.price = data['price']
             db.session.commit()
             return make_response(jsonify({'message': 'book updated'}), 200)
         return make_response(jsonify({'message': 'book not found'}), 404)
@@ -80,7 +96,7 @@ def update_book(id):
         return make_response(jsonify({'message': 'error updating book'}), 500)
 
 # delete a book
-@app.route('/books/<int:id>', methods = ['DELETE'])
+@app.route('/api/product/delete-book/<int:id>', methods = ['DELETE'])
 def delete_book(id):
     try:
         book = Book.query.filter_by(id=id).first()
@@ -91,3 +107,8 @@ def delete_book(id):
         return make_response(jsonify({'message': 'book not found'}), 404)
     except Exception as e:
         return make_response(jsonify({'message': 'error deleting book'}), 500)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
