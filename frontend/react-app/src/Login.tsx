d3529cc5f5da316a6113f5e740d532d6cc69d5fc
zamb0 <zamboni.fabio23@gmail.com>
2024-05-15 23:01:49 +0200

    working login

--- frontend/react-app/src/Login.tsx
+++ frontend/react-app/src/Login.tsx
@@ -6,13 +6,26 @@ import {
   CardHeader,
   CardTitle,
 } from "./components/ui/card"
-import { Input } from "./components/ui/input"
-import { Label } from "./components/ui/label"
-import { Link } from "react-router-dom";
+import LoginForm from "./LoginForm"
+import {useNavigate} from "react-router-dom"
+import App from "./App"
+import { useEffect } from "react"
+
+import "./Login.css"
+
 
 function Login() {
+
+    let navigate = useNavigate()
+
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            navigate("/")
+        }
+      }, []);
+
   return (
-    <Card className="mx-auto max-w-sm">
+    <Card className="cardLogin">
       <CardHeader>
         <CardTitle className="text-2xl">Login</CardTitle>
         <CardDescription>
@@ -20,38 +33,7 @@ function Login() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link className="ml-auto inline-block text-sm underline" to={"http://google.com"}>
-                Forgot your password?
-              </Link>
-            </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-          <Button variant="outline" className="w-full">
-            Login with Google
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link className="underline" to={"http://google.com"}>
-            Sign up
-          </Link>
-        </div>
+        <LoginForm />
       </CardContent>
     </Card>
   )
---
+++ frontend/react-app/src/LoginForm.tsx
@@ -0,0 +1,162 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import axios from "axios";
+import { ReactElement, useEffect } from "react";
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "./components/ui/form";
+
+import { useState } from "react";
+import { useBackend } from "./services/backendService";
+import { useNavigate } from "react-router-dom";
+import AlertError from "./AlertError";
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(2, {
+        message: "Password must be at least 2 characters.",
+    }),
+});
+
+interface LogInProps {
+    onSubmit: (formData: z.infer<typeof formSchema>) => void;
+}
+
+const LoginForm: React.FC<any> = () => {
+    const [token, setToken] = useState<string | null>(null);
+    let [credentialError, setCredentialError] = useState<string | null>(null);
+    let [backendError, setBackendError] = useState<string | null>(null);
+
+    //const backend = useBackend();
+    let navigate = useNavigate();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+    
+    const onSubmitHandler = async (formData: z.infer<typeof formSchema>) => {
+        try {
+            // Validate form data against the schema
+            console.log("Form data is valid:", JSON.stringify(formData));
+            const responce = await axios.post(
+                "http://127.0.0.1:4001/api/account/login",
+                JSON.stringify(formData),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            console.log(responce);
+            setToken(responce.data.token);
+            localStorage.setItem("token", responce.data.token);
+            navigate("/");
+            
+            // Optionally, you can redirect the user or show a success message
+        } catch (error: any) {
+            if (error instanceof z.ZodError) {
+                console.error("Validation failed:", error.errors);
+                setCredentialError("Invalid credentials");    
+                setCredentialError("Invalid credentials");
+                // Optionally, you can handle validation errors, show error messages, etc.
+            } else {
+                console.error("Error submitting form:", error);
+                setBackendError(error.response.data.message);
+                // Optionally, you can handle other types of errors
+            }
+        }
+    };
+
+    // // 2. Define a submit handler.
+    // function onSubmit(values: z.infer<typeof formSchema>) {
+    //     setCredentialError(null);
+
+    //     try {
+    //         backend
+    //             .loginAPI(values.username, values.username, values.password)
+    //             .then(([token, message]) => {
+    //                 setToken(token);
+    //                 setMessage(message);
+    //             });
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+
+    //     if (token) {
+    //         localStorage.setItem("token", token);
+    //     } else {
+    //         setCredentialError("Invalid credentials");
+    //     }
+
+    //     if (token) {
+    //         navigate("/");
+    //     }
+    // }
+
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmitHandler)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username or Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder=""
+                                    type="password"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>Your password</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+            {backendError ? <AlertError message = {backendError}/> : null}
+            {credentialError ? <AlertError message = {credentialError}/> : null}
+        </Form>
+    );
+}
+
+/*
+ {credentialError ? <AlertLogin/> : null}
+*/
+
+export default LoginForm;
