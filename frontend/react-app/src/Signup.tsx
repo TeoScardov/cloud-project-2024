07be4a63bdb47653e3617a08022311e4ce0f8f8c
zamb0 <zamboni.fabio23@gmail.com>
2024-05-16 01:10:15 +0200

    deleted useless import

--- frontend/react-app/src/Signup.tsx
+++ frontend/react-app/src/Signup.tsx
@@ -1,4 +1,3 @@
-import { Button } from "./components/ui/button";
 import {
     Card,
     CardContent,
@@ -6,10 +5,6 @@ import {
     CardHeader,
     CardTitle,
 } from "./components/ui/card";
-import { Input } from "./components/ui/input";
-import { Label } from "./components/ui/label";
-
-import { Link } from "react-router-dom";
 
 import "./Signup.css";
 import SignupForm from "./SignupForm";
