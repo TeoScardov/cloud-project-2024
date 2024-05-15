46b949a2dabf2b2becfa13d7a527e3b43a887f53
zamb0 <zamboni.fabio23@gmail.com>
2024-05-15 23:00:50 +0200

    some modules and fix

--- frontend/react-app/src/main.tsx
+++ frontend/react-app/src/main.tsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BackendProvider } from './services/backendService'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Router>
   <React.StrictMode>
-     <BackendProvider>
      <App />
-    </BackendProvider>
-  </React.StrictMode>
-  </Router>,
+  </React.StrictMode>,
 )
