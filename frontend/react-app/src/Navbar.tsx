b0b683f01943155ddf5d07e8289a256bf944adfa
zamb0 <zamboni.fabio23@gmail.com>
2024-05-15 23:05:19 +0200

    working navbar

--- frontend/react-app/src/Navbar.tsx
+++ frontend/react-app/src/Navbar.tsx
@@ -15,22 +15,39 @@ import {
 } from "./components/ui/menubar";
 
 import Cart from "./Cart";
+import { useNavigate } from "react-router-dom";
+import { Menu } from "lucide-react";
+
 
 function Navbar() {
+    let navigate = useNavigate();
+
+    const isAuth = localStorage.getItem("token") !== null;
+
     return (
         <Menubar>
             <MenubarMenu>
-                <MenubarTrigger>Home</MenubarTrigger>
+                <MenubarTrigger onClick={() => navigate("/")}>
+                    Home
+                </MenubarTrigger>
             </MenubarMenu>
             <div className="flex-grow"></div>
             <MenubarMenu>
                 <MenubarTrigger>Profile</MenubarTrigger>
+                {isAuth ? (
                 <MenubarContent>
-                    <MenubarItem>Log in</MenubarItem>
-                    <MenubarItem>Log out</MenubarItem>
-                    <MenubarItem>Sign up</MenubarItem>
                     <MenubarItem>Library</MenubarItem>
-                </MenubarContent>
+                    <MenubarItem onClick={() => {navigate("/"); localStorage.removeItem("token")}}>Log out</MenubarItem>
+                </MenubarContent>) : (
+                <MenubarContent>
+                    <MenubarItem onClick={() => navigate("/login")}>
+                        Log in
+                    </MenubarItem>
+                    <MenubarItem onClick={() => navigate("/signup")}>
+                        Sign up
+                    </MenubarItem>
+                    </MenubarContent>
+                    )}
             </MenubarMenu>
             <MenubarMenu>
                 <Cart />
