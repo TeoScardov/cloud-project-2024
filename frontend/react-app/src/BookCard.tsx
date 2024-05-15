b61b9a63080ac6f9f41fae52bee6f4d965767030
zamb0 <zamboni.fabio23@gmail.com>
2024-05-16 01:08:12 +0200

    add toaster

--- frontend/react-app/src/BookCard.tsx
+++ frontend/react-app/src/BookCard.tsx
@@ -18,8 +18,23 @@ import {
 } from "./components/ui/select";
 
 import "./BookCard.css"
+import { useToast } from "./components/ui/use-toast"
 
 function BookCard(props: { title: string; description: string; price: number; }) {
+
+    const { toast } = useToast()
+
+    const handleClick = () => {
+        
+        toast({
+            title: props.title + " added to cart!",
+            //description: "You can view your cart by clicking the cart icon in the top right corner.",
+          })
+
+        console.log('Button clicked!');
+        // You can add any other logic you need here
+      };
+
     return (
         <Card className="card">
             <CardHeader>
@@ -43,7 +58,7 @@ function BookCard(props: { title: string; description: string; price: number; })
             </CardContent>
             <CardFooter className="flex justify-between">
                 <Button className="card_button" variant="outline">Details</Button>
-                <Button className="card_button">Add to Cart</Button>
+                <Button className="card_button" onClick={handleClick}>Add to Cart</Button>
             </CardFooter>
         </Card>
     );
