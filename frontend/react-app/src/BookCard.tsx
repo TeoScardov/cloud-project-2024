f5d1d4805bbf51d72bf53e1252eaf8a58b0b59de
zamb0 <zamboni.fabio23@gmail.com>
2024-05-17 13:39:01 +0200

    connected to api (not fully working)

--- frontend/react-app/src/BookCard.tsx
+++ frontend/react-app/src/BookCard.tsx
@@ -19,33 +19,73 @@ import {
 
 import "./BookCard.css"
 import { useToast } from "./components/ui/use-toast"
+import { Book } from "./TableCartBook";
+import Cookies from 'universal-cookie';
 
-function BookCard(props: { title: string; description: string; price: number; }) {
+function BookCard(props: Book) {
 
     const { toast } = useToast()
+    const cookies = new Cookies();
 
-    const handleClick = () => {
-        
-        toast({
-            title: props.title + " added to cart!",
-            //description: "You can view your cart by clicking the cart icon in the top right corner.",
-          })
+    const handleClick = async () => {
 
-        console.log('Button clicked!');
-        // You can add any other logic you need here
-      };
+
+        let cart_id = null
+
+        if (localStorage.getItem("cart_id")) {
+
+            cart_id = localStorage.getItem("cart_id")
+        }
+
+        try {
+            let response = await fetch('http://0.0.0.0:4005/api/cart/addProduct', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    product_id: props.isbn,
+                    cart_id: cart_id
+                }),
+            });
+
+            console.log(response)
+            const cookie = response.headers.get('Set-Cookie')
+            console.log(cookie)
+            //cookies.set("cart_id", cookie., { path: '/' });
+            //console.log(cookies.get('myCat')); // Pacman
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const data = await response.json();
+
+            console.log(data);
+    
+            toast({
+                title: props.title + " added to cart!",
+                //description: "You can view your cart by clicking the cart icon in the top right corner.",
+            });
+    
+            console.log('Button clicked!');
+            // You can add any other logic you need here
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
 
     return (
         <Card className="card">
             <CardHeader>
-                <img src="https://via.placeholder.com/150" alt="Book cover" />
+                <img src="https://placehold.co/150x150/png" alt="Book cover" />
             </CardHeader>
             <CardContent>
                 <div>
                     <CardTitle className="text-xl">{props.title}</CardTitle>
-                    <CardDescription>
+                    {/* <CardDescription>
                         {props.description}
-                    </CardDescription>
+                    </CardDescription> */}
                 </div>
                 <div className="grid gap-4">
                 </div>    
@@ -56,6 +96,7 @@ function BookCard(props: { title: string; description: string; price: number; })
                 </div>
                
             </CardContent>
+            <div className="flex justify-center"/>
             <CardFooter className="flex justify-between">
                 <Button className="card_button" variant="outline">Details</Button>
                 <Button className="card_button" onClick={handleClick}>Add to Cart</Button>
--- frontend/react-app/src/BooksHome.tsx
+++ frontend/react-app/src/BooksHome.tsx
@@ -1,7 +1,26 @@
 import BookCard from "./BookCard";
 import { ScrollArea, ScrollBar } from "./components/ui/scroll-area";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "./components/ui/carousel";
+import { useEffect, useState } from "react";
+import { Book } from "./TableCartBook";
 
 function BooksHome() {
+    const [bookList, setBookList] = useState<Array<Book>>([]);
+
+    useEffect(() => {
+        fetch("http://0.0.0.0:4004/api/product/get-books")
+            .then((response) => response.json())
+            .then((data) => {
+                setBookList(data);
+            });
+    }, []);
+
     return (
         // <ScrollArea className="h-100vh w-[350px] rounded-md border p-4">
         //     <div className="flex justify-center items-center">
@@ -12,21 +31,57 @@ function BooksHome() {
         //         <BookCard title="Title5" description="ciao5" price={50} />
         //     </div>
         // </ScrollArea>
-        <ScrollArea className="h-100vh w-100ow whitespace-nowrap rounded-md border">
-            <div className="flex w-max space-x-10 p-4">
-                <BookCard title="Title1" description="ciao1" price={10} />
-                <BookCard title="Title2" description="ciao2" price={20} />
-                <BookCard title="Title3" description="ciao3" price={30} />
-                <BookCard title="Title4" description="ciao4" price={40} />
-                <BookCard title="Title5" description="ciao5" price={50} />
-                <BookCard title="Title1" description="ciao1" price={10} />
-                <BookCard title="Title2" description="ciao2" price={20} />
-                <BookCard title="Title3" description="ciao3" price={30} />
-                <BookCard title="Title4" description="ciao4" price={40} />
-                <BookCard title="Title5" description="ciao5" price={50} />
-            </div>
-            <ScrollBar orientation="horizontal" />
-        </ScrollArea>
+
+        // 50% on small screens and 33% on larger screens.
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "60vh",
+            }}
+        >
+            <Carousel
+                opts={{
+                    align: "start",
+                }}
+                className="w-[100%] max-w-[80%]"
+            >
+                <CarouselPrevious />
+                <CarouselContent className="-ml-1">
+                    {bookList.map((book) => (
+                        <CarouselItem className="md:basis-1/2 lg:basis-1/4" key={book.isbn}>
+                            <div className="p-1" key={book.isbn}>
+                                <BookCard
+                                    key={book.isbn}
+                                    title={book.title}
+                                    description={book.description}
+                                    price={book.price}
+                                    isbn={book.isbn}
+                                />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselNext />
+            </Carousel>
+        </div>
+
+        // <ScrollArea className="h-100vh w-100ow whitespace-nowrap rounded-md border">
+        //     <div className="flex w-max space-x-10 p-4">
+        //         <BookCard title="Title1" description="ciao1" price={10} />
+        //         <BookCard title="Title2" description="ciao2" price={20} />
+        //         <BookCard title="Title3" description="ciao3" price={30} />
+        //         <BookCard title="Title4" description="ciao4" price={40} />
+        //         <BookCard title="Title5" description="ciao5" price={50} />
+        //         <BookCard title="Title1" description="ciao1" price={10} />
+        //         <BookCard title="Title2" description="ciao2" price={20} />
+        //         <BookCard title="Title3" description="ciao3" price={30} />
+        //         <BookCard title="Title4" description="ciao4" price={40} />
+        //         <BookCard title="Title5" description="ciao5" price={50} />
+        //     </div>
+        //     <ScrollBar orientation="horizontal" />
+        // </ScrollArea>
 
         //     <ScrollArea className="w-96 whitespace-nowrap rounded-md border">
         //     <div className="flex w-max space-x-4 p-4">
