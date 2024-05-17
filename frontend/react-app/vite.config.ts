cbd2e9b4074a2e8e2e0e076c2d622a8ff90c4129
zamb0 <zamboni.fabio23@gmail.com>
2024-05-17 13:34:00 +0200

    added HTTPS

--- frontend/react-app/vite.config.ts
+++ frontend/react-app/vite.config.ts
@@ -1,9 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import mkcert from 'vite-plugin-mkcert'
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // plugins: [react(),mkcert()],
   plugins: [react()],
   resolve: {
     alias: {
