3049e2996fddbbeb8f9e383a72facc0bd083e7e2
zamb0 <zamboni.fabio23@gmail.com>
2024-05-23 18:10:12 +0200

    fix purchase process and add getPurchaseByAccountId

--- docker/purchase_service/purchaseApp/controller.py
+++ docker/purchase_service/purchaseApp/controller.py
@@ -1,6 +1,7 @@
 import requests
-from flask import jsonify, make_response
+from flask import jsonify, make_response, Response
 from purchaseApp.models import PaymentDao, PurchaseDao, PurchaseItemDao
+
 
 def isAuthenticated(request):
     """
@@ -40,7 +41,7 @@ def isAuthenticated(request):
         
         return response # type: ignore
 
-def createNewPurchase(purchase_data, account_id):
+def createNewPurchase(purchase_data, account_id)-> [Response, PurchaseDao]: # type: ignore
     """
     This function is used to create a new purchase in the database.
     It sends a POST request to the purchase service with the purchase data.
@@ -58,31 +59,37 @@ def createNewPurchase(purchase_data, account_id):
     """
     try:
 
-        total_price = purchase_data['cart']['total_price']
+        total = purchase_data['cart']['total']
 
-        purchase = PurchaseDao(account_id=account_id, total_price=total_price, status="PENDING")
+        purchase = PurchaseDao(account_id=account_id, total_price=total, status="PENDING")
 
         purchase.save()
 
-        return jsonify({
+        # return jsonify({
+        #     'status': 'success',
+        #     'message': 'Purchase created',
+        #     #'purchase': jsonify(purchase.to_dict())
+        # }), 200
+
+        return [make_response(jsonify({
             'status': 'success',
             'message': 'Purchase created',
-            'purchase': purchase
-        }), 200
+            'purchase': purchase.to_dict()
+        }), 200), purchase]
     
     except Exception as e:
         print(e)
 
         purchase.rollback()
 
-        return jsonify({
+        return make_response(jsonify({
             'status': 'error',
-            'message': 'Purchase failed',
+            'message': 'Purchase creation failed',
             'error': e
-        }), 500
+        }), 500)
 
 
-def performPayment(purchase, auth_token):
+def performPayment(purchase, purchase_data, auth_token)-> [Response, PaymentDao]: # type: ignore
     """
     This function is used to perform a payment for a purchase.
     It sends a POST request to the payment service with the purchase id.
@@ -91,7 +98,7 @@ def performPayment(purchase, auth_token):
 
     :param
     purchase: PurchaseDao The purchase object for which the payment is to be made.
-    auth_token: str The authentication token for the user making the purchase.
+    purchase_data: dict The purchase data containing the payment details.
 
     Returns:
     The response from the payment service.
@@ -101,8 +108,13 @@ def performPayment(purchase, auth_token):
 
     SERVICE = "http://payment_service:4000/api/payment/"
 
-    #data_payment = purchase[]
-    data_payment = {"amount": 30, "card" : 3344567826351444, "cvv" : 366, "expiration" : "12/27", "billing_address": "Via Ciao Ciao 36"}
+    data_payment = {
+        'total': purchase_data['cart']['total'],
+        'billing_address': purchase_data['billing_address'],
+        'cc': purchase_data['cc'],
+        'expiredate': purchase_data['expiredate'],
+        'cvv': purchase_data['cvv'],
+    }
 
     try:
 
@@ -110,10 +122,11 @@ def performPayment(purchase, auth_token):
         payment_responce = requests.post(SERVICE, json=data_payment, headers={'Authorization': auth_token})
 
         if payment_responce.status_code != 200:
-            return jsonify({
+            return make_response(jsonify({
                 'status': 'error',
-                'message': 'Payment failed'
-            }), 500
+                'message': 'Payment failed',
+                'error': payment_responce.json()
+            }), 500)
         
         payment = PaymentDao(purchase_id=purchase.id)
         purchase.status = "APPROVED"
@@ -121,11 +134,17 @@ def performPayment(purchase, auth_token):
 
         payment.save()
 
-        return jsonify({
+        # return jsonify({
+        #     'status': 'success',
+        #     'message': 'Payment successful',
+        #     #'payment': payment
+        # }), 200
+
+        return [make_response(jsonify({
             'status': 'success',
             'message': 'Payment successful',
-            'payment': payment
-        }), 200
+            'payment': payment.to_dict()
+        }), 200), payment]
     
     except Exception as e:
 
@@ -133,12 +152,11 @@ def performPayment(purchase, auth_token):
         purchase.status = "REJECTED"
         purchase.save()
 
-        print(e)
-        return jsonify({
+        return make_response(jsonify({
             'status': 'error',
             'message': 'Payment failed',
             'error': e
-        }), 500
+        }), 500)
 
 
 def associateBooksToPurchase(purchase, purchase_data):
@@ -159,34 +177,34 @@ def associateBooksToPurchase(purchase, purchase_data):
     """
 
     try:
-        purchase_id = purchase.id
+        purchase_id = purchase.id,
 
         list_of_books = []
 
-        for book in purchase_data['cart']['books']:
+        for book in purchase_data['cart']['items']:
             
-            purchase_item = PurchaseItemDao(order_id=purchase_id, product_id=book['id'])
+            purchase_item = PurchaseItemDao(order_id=purchase_id, product_id=book['isbn'])
             list_of_books.append(purchase_item)
 
         for item in list_of_books:
             item.save()
 
-        return jsonify({
+        
+        return make_response(jsonify({
             'status': 'success',
             'message': 'Books associated to purchase'
-        }), 200
+        }), 200)
 
     except Exception as e:
 
         for item in list_of_books:
             item.rollback()
 
-        print(e)
-        return jsonify({
+        return make_response(jsonify({
             'status': 'error',
             'message': 'Associate books to purchase failed',
             'error': e
-        }), 500
+        }), 500)
     
 
 def associateBooksToAccount(account_id, purchase_data, auth_token):
@@ -211,9 +229,9 @@ def associateBooksToAccount(account_id, purchase_data, auth_token):
     books = []
     try:
 
-        for book in purchase_data['cart']['books']:
+        for book in purchase_data['cart']['items']:
             
-            books.append(book['id'])
+            books.append(book['isbn'])
 
         data = {
             'account_id': account_id,
@@ -233,11 +251,11 @@ def associateBooksToAccount(account_id, purchase_data, auth_token):
     except Exception as e:
         
         print(e)
-        return jsonify({
+        return make_response(jsonify({
             'status': 'error',
             'message': 'Associate books to account failed',
             'error': e
-        }), 500
+        }), 500)
     
 
 def clearCart(cart_id, auth_tocken):
@@ -274,3 +292,23 @@ def clearCart(cart_id, auth_tocken):
         
         return response
     
+
+def getPurchaseByAccountId(account_id):
+    """
+    This function is used to get all purchases made by a user.
+    It sends a GET request to the purchase service with the user_id.
+    If the purchases are retrieved successfully, it returns the response from the purchase service.
+    If the retrieval fails, it returns a JSON response with an error message and a status code of 500.
+
+    :param
+    user_id: str The unique identifier of the user.
+
+    Returns:
+    The response from the purchase service.
+    or
+    A JSON response with an error message and a status code of 500.
+    """
+
+    purchases = PurchaseDao.get_by_account_id(account_id=account_id)
+
+    return purchases
