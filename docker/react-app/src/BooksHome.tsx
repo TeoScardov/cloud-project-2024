a6142ba03b67884216acd72f1e1ffadf03cfc926
zamb0 <zamboni.fabio23@gmail.com>
2024-05-23 23:10:04 +0200

    remove background

--- docker/react-app/src/BooksHome.tsx
+++ docker/react-app/src/BooksHome.tsx
@@ -29,7 +29,6 @@ function BooksHome() {
 
     return (
         <div className="flex min-h-screen w-full flex-col">
-            <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
                 <div
                     style={{
                         display: "flex",
@@ -60,7 +59,6 @@ function BooksHome() {
                         <CarouselNext />
                     </Carousel>
                 </div>
-            </main>
         </div>
     );
 }
--- docker/react-app/src/Profile.tsx
+++ docker/react-app/src/Profile.tsx
@@ -57,7 +57,7 @@ function Profile() {
 
     return (
         <div className="flex min-h-screen w-full flex-col">
-            <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+            <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
                 <div className="mx-auto grid w-full max-w-6xl gap-2">
                     <h1 className="text-3xl font-semibold">Profile</h1>
                 </div>
