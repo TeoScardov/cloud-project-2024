ac2c163e77e01bad9ced45b1c9b3434bfe75c58e
zamb0 <zamboni.fabio23@gmail.com>
2024-05-23 18:15:49 +0200

    use backend

--- frontend/react-app/src/CartItems.tsx
+++ frontend/react-app/src/CartItems.tsx
@@ -1,101 +1,77 @@
 import { Book, columns } from "./TableCartBook";
-import { DataTable } from "./TableCartData";
+import { DataTable } from "./TableCartBookData";
 import { Suspense, useEffect, useState } from "react";
 import { Card } from "./components/ui/card";
-import { Cookies } from "react-cookie";
-import axios from "axios";
+import { useBackend } from "./services/backendService";
 
-export default function CartItems() {
-    const [dataBook, setDataBook] = useState<Book[]>([]);
-    let cart_books = useState<Book[]>([])[0];
+export default function CartItems(props:{updateCartIdState: (arg0: boolean) => void, updateCartEmptyState: (arg0: boolean) => void}) {
+    const [dataBook, setDataBook] = useState<Book[] | null>(null);
+    const backend = useBackend();
 
-    const cookies = new Cookies();
-
-    const getData = async () => {
-
-        let cart_id = null;
-
-        if (cookies.get("cart_id")) {
-            cart_id = cookies.get("cart_id");
-        }
-
-        var responce = await fetch(`http://0.0.0.0:4005/api/cart/show_cart?cart_id=${cart_id}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }
-        );
-        var json = await responce.json();
-
-        console.log(json);
-
-        json = JSON.parse(json.body);
-
-        console.log(json);
-
-        cart_books = json["items"];
-
-        if (cart_books === null) {
-            cart_books = [];
-        }
-
-        setDataBook(cart_books);
-    };
+    const updateCartIdState = props.updateCartIdState;
+    const updateCartEmptyState = props.updateCartEmptyState;
 
     useEffect(() => {
-        
-        if (!cookies.get("cart_id")) {
+        if (!localStorage.getItem("cart_id")) {
             console.log("No cart_id found");
 
             return;
         } else {
+            //getData()
+            backend.getCartItems().then((data: any) => {
+                //console.log(data.items);
+                setDataBook(data.items);
+                updateCartIdState(true);
 
-        getData()
-
+                // if (data.items.length === 0 || data.items === null) {
+                if (data.items === null) {
+                    updateCartEmptyState(true);
+                } else {
+                    updateCartEmptyState(false);
+                }
+            });
         }
-
     }, []);
 
-    const handleClickDelete = async (product_id: any) => {
-        const cookies = new Cookies();
-        
-        console.log("Delete book with isbn", product_id)
+    const handleClickDelete = (isbn: string) => {
+        backend.deleteCartItem(isbn).then(() => {
+            backend.getCartItems().then((data: any) => {
+                setDataBook(data.items);
 
-        try {
-          const response = await axios.delete(`http://0.0.0.0:4005/api/cart/removeProduct`, { data: { product_id: product_id, cart_id: cookies.get("cart_id")} });
-
-          console.log("Book deleted", response.data);
-
-            getData()
-
-      } catch (error) {
-          console.error("Error deleting book", error);
-      }
-      }
-
+                // if (data.items.length === 0) {
+                if (data.items === null) {
+                    updateCartEmptyState(true);
+                } else {
+                    updateCartEmptyState(false);
+                }
+            });
+        });
+    };
 
     return (
         <div>
-            {!cookies.get("cart_id") || dataBook.length===0 ? (
+            {!localStorage.getItem("cart_id") || dataBook === null ? (
                 <Card>
-                <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "60vh",
-                    fontSize: "1.5em",
-                    color: "#888",
-                }}
-            >
-                Empty cart
-            </div>
-            </Card>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            height: "60vh",
+                            fontSize: "1.5em",
+                            color: "#888",
+                        }}
+                    >
+                        Empty cart
+                    </div>
+                </Card>
             ) : (
-                <DataTable columns={columns} data={dataBook} handleClickDelete={handleClickDelete}/>
+                <DataTable
+                    columns={columns}
+                    data={dataBook}
+                    handleClickDelete={handleClickDelete}
+                />
             )}
         </div>
     );
-
 }
