f2ab0ee8e5c7dd595e4a40eaaddde675aa479a56
zamb0 <zamboni.fabio23@gmail.com>
2024-05-24 19:48:08 +0200

    add scroll bar

--- docker/react-app/src/Library.tsx
+++ docker/react-app/src/Library.tsx
@@ -1,44 +1,52 @@
-import { Scroll } from "lucide-react";
 import LibraryCard from "./LibraryCard";
 import { Book } from "./TableCartBook";
-import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
 
 function Library(props: { library: Array<Book["isbn"]> }) {
     const library = props.library;
 
-    return library.length === 0 ? (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "60vh",
-                fontSize: "1.5em",
-                color: "#888",
-            }}
-        >
-            e No books available
-        </div>
-    ) : (
-        <div style={{ overflowY: "auto", maxHeight: "500px" }}>
-            <ScrollArea>
-            <div
-                style={{
-                    display: "grid",
-                    gridTemplateColumns:
-                        "repeat(auto-fill, minmax(200px, 1fr))",
-                    gap: "1rem",
-                }}
-            >
-                {library.map((data, index) => (
-                    <div key={index}>
-                        <LibraryCard isbn={data} />
+    return (
+        <Card x-chunk="dashboard-04-chunk-1">
+            <CardHeader>
+                <CardTitle>Library</CardTitle>
+            </CardHeader>
+            <CardContent>
+                {library.length === 0 ? (
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            height: "60vh",
+                            fontSize: "1.5em",
+                            color: "#888",
+                        }}
+                    >
+                        No eBooks available
                     </div>
-                ))}
-            </div>
-            </ScrollArea>
-        </div>
-
+                ) : (
+                        <div
+                            style={{
+                                display: "grid",
+                                gridTemplateColumns:
+                                    "repeat(auto-fill, minmax(200px, 1fr))",
+                                gap: "1rem",
+                                overflow: "auto",
+                                maxHeight: "600px",
+                                padding: "1rem",
+                                borderRadius: "10px",
+                                //boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+                            }}
+                        >
+                            {library.map((data, index) => (
+                                <div key={index}>
+                                    <LibraryCard isbn={data} />
+                                </div>
+                            ))}
+                        </div>
+                )}
+            </CardContent>
+        </Card>
     );
 }
 
