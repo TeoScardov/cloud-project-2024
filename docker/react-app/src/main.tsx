a11b6f1b38e06c4a4e981d1e9d0bf9d7350b662c
zamb0 <zamboni.fabio23@gmail.com>
2024-05-30 19:45:20 +0200

    wrapping the main with Backend

--- docker/react-app/src/main.tsx
+++ docker/react-app/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BackendProvider } from "./services/backendService";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-     <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <BackendProvider>
+            <App />
+        </BackendProvider>
+    </React.StrictMode>
+);
