660657159b3bd8b368dd21cec9766beeb563f3b6
zamb0 <zamboni.fabio23@gmail.com>
2024-05-30 19:45:56 +0200

    add check for token in localstorage

--- docker/react-app/src/App.tsx
+++ docker/react-app/src/App.tsx
@@ -1,39 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-
 import Navbar from "./Navbar";
-import BookCard from "./BookCard";
 import Login from "./Login";
 import Signup from "./Signup";
 import BooksHome from "./BooksHome";
-import { BackendProvider } from "./services/backendService";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
 import Profile from "./Profile";
 import { ThemeProvider } from "./ThemeProvider";
 import Checkout from "./Checkout";
-
+import { useBackend } from "./services/backendService";
+import { useEffect } from "react";  
 
 function App() {
+    const backend = useBackend();
+
+    useEffect(() => {
+        if (localStorage.getItem("token") !== null) {
+            backend.getAuth();
+        }
+    }, []);
 
     return (
-        <BackendProvider>
-            <BrowserRouter>
+        <BrowserRouter>
             <ThemeProvider>
-            <Navbar />
+                <Navbar />
                 <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-                <Routes>
-                    <Route path="/" element={<BooksHome />} />
-                    <Route path="/login" element={ <Login/> } />
-                    <Route path="/signup" element={ <Signup />} />
-                    <Route path="/profile" element={ <Profile />} />
-                    <Route path="/checkout" element={ <Checkout />} />
-                </Routes>
+                    <Routes>
+                        <Route path="/" element={<BooksHome />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/checkout" element={<Checkout />} />
+                    </Routes>
                 </main>
                 <Toaster />
             </ThemeProvider>
-            </BrowserRouter>
-         </BackendProvider>
+        </BrowserRouter>
     );
 }
 
