0d8489333abd4ccef5fed4353bfb5fb3acdc2f9d
zamb0 <zamboni.fabio23@gmail.com>
2024-06-01 15:18:22 +0200

    add api documentation with flasgger

--- docker/payment_service/paymentApp/__init__.py
+++ docker/payment_service/paymentApp/__init__.py
@@ -1,6 +1,8 @@
+from re import S
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flasgger import Swagger
 from os import environ
 
 try:
@@ -13,9 +15,17 @@ except Exception as e:
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SWAGGER'] = {
+    'title': 'Payment Service API',
+    'uiversion': 3,
+    'specs_route': '/api/docs/',
+    'version': '1.0',
+    'description': 'API for Payment Service',
+}
 
 jwt = JWTManager(app)
 CORS(app)
+Swagger(app)
 
 from paymentApp.views import blueprint
 app.register_blueprint(blueprint, url_prefix=URL_PREFIX)
