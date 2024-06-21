e50ab1ffab39c767d9b867de95e039aad282c473
zamb0 <zamboni.fabio23@gmail.com>
2024-06-21 17:06:44 +0200

    refactor for automated testing

---
+++ docker/shopping_cart/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from app import create_app
+from cartApp.db import db
+
+@pytest.fixture()
+def app():
+    app = create_app("testing")
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all() 
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
