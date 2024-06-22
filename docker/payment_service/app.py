98f5bf5aba196505c2fa96f5ec586dc885bfda57
zamb0 <zamboni.fabio23@gmail.com>
2024-06-22 19:57:46 +0200

    payment refactor

--- docker/payment_service/app.py
+++ docker/payment_service/app.py
@@ -1,9 +1,28 @@
-from os import environ
-from paymentApp import app
+import logging
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from flasgger import Swagger
+from paymentApp.config import *
 
-DEBUG = environ.get("DEBUG", "False").lower() == "true"
+def create_app(config_class="production"):
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    app = Flask(__name__)
+    app.config.from_object(get_config_map(config_class))
+    
+    from paymentApp.views import blueprint
+    app.register_blueprint(blueprint, url_prefix=Config.URL_PREFIX)
+
+    if config_class == "production":
+        JWTManager(app)
+        CORS(app)
+        Swagger(app)
+            
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG)
+    app = create_app()
+    app.run(debug=Config.DEBUG)
 
 
