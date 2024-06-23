b6bdb63d93ba9a670346c9a381e7c12f55c982dc
zamb0 <zamboni.fabio23@gmail.com>
2024-06-23 12:55:48 +0200

    switch to String type instead of UUID

--- docker/purchase_service/purchaseApp/models.py
+++ docker/purchase_service/purchaseApp/models.py
@@ -1,6 +1,6 @@
 import uuid
 from purchaseApp.database import db
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String
 
 class PurchaseDao(db.Model):
     """
@@ -35,8 +35,8 @@ class PurchaseDao(db.Model):
 
     __tablename__ = 'orders'
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True)
-    account_id = db.Column(UUID(as_uuid=True), nullable=False)
+    id = db.Column(String(38), primary_key=True)
+    account_id = db.Column(String(38), nullable=False)
     order_date = db.Column(db.DateTime, nullable=False)
     total = db.Column(db.Float, nullable=False)
     status = db.Column(db.Enum('PENDING', 'APPROVED', 'REJECTED', name='status_enum'), nullable=False)
@@ -57,7 +57,7 @@ class PurchaseDao(db.Model):
             the status of the purchase
         """
 
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.account_id = account_id
         self.order_date = db.func.current_timestamp()
         self.total = total
@@ -100,21 +100,21 @@ class PurchaseDao(db.Model):
         """
         Returns all purchases in the database
         """
-        return PurchaseDao.query.all()
+        return db.session.query(PurchaseDao).all()
     
     @staticmethod
     def get_by_id(id):
         """
         Returns the purchase with the given id
         """
-        return PurchaseDao.query.get(id)
+        return db.session.query(PurchaseDao).filter_by(id=id).first()
 
     @staticmethod
     def get_by_account_id(account_id):
         """
         Returns all purchases with the given account id
         """
-        return PurchaseDao.query.filter_by(account_id=account_id).all()
+        return db.session.query(PurchaseDao).filter_by(account_id=account_id).all() 
     
     
 class PurchaseItemDao(db.Model):
@@ -148,8 +148,8 @@ class PurchaseItemDao(db.Model):
 
     __tablename__ = 'order_items'
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True)
-    order_id = db.Column(UUID(as_uuid=True), db.ForeignKey('orders.id'), nullable=False, unique=True)
+    id = db.Column(String(38), primary_key=True)
+    order_id = db.Column(String(38), db.ForeignKey('orders.id'), nullable=False, unique=True)
     product_id = db.Column(db.JSON, nullable=False)
 
     def __init__(self, order_id, product_id):
@@ -165,8 +165,7 @@ class PurchaseItemDao(db.Model):
         product_id : str
             a unique identifier for the product
         """
-        #self.id = str(uuid.uuid4())
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.order_id = order_id
         self.product_id = product_id
 
@@ -205,21 +204,21 @@ class PurchaseItemDao(db.Model):
         """
         Returns all purchase items in the database
         """
-        return PurchaseItemDao.query.all()
+        return db.session.query(PurchaseItemDao).all()
     
     @staticmethod
     def get_by_id(id):
         """
         Returns the purchase item with the given id
         """
-        return PurchaseItemDao.query.get(id)
+        return db.session.query(PurchaseItemDao).filter_by(id=id).first()
     
     @staticmethod
     def get_by_order_id(order_id):
         """
         Returns all purchase items with the given order id
         """
-        return PurchaseItemDao.query.filter_by(order_id=order_id).one_or_none()
+        return db.session.query(PurchaseItemDao).filter_by(order_id=order_id).first()
 
 class PaymentDao(db.Model):
     """
@@ -246,8 +245,8 @@ class PaymentDao(db.Model):
 
     __tablename__ = 'payment'
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True)
-    purchase_id = db.Column(UUID(as_uuid=True), db.ForeignKey('orders.id'), nullable=False, unique=True)
+    id = db.Column(String(38), primary_key=True)
+    purchase_id = db.Column(String(38), db.ForeignKey('orders.id'), nullable=False, unique=True)
 
     def __init__(self, purchase_id):
         """
@@ -258,7 +257,7 @@ class PaymentDao(db.Model):
         purchase_id : str
             a unique identifier for the purchase
         """
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.purchase_id = purchase_id
 
     def save(self):
@@ -292,11 +291,11 @@ class PaymentDao(db.Model):
         """
         Returns the payment with the given id
         """
-        return PaymentDao.query.filter_by(id=payment_id).first()
+        return db.session.query(PaymentDao).filter_by(id=payment_id).first()
 
     @staticmethod
     def get_by_purchase_id(purchase_id):
         """
         Returns the payment with the given purchase id
         """
-        return PaymentDao.query.filter_by(purchase_id=purchase_id).first()
+        return db.session.query(PaymentDao).filter_by(purchase_id=purchase_id).first()
