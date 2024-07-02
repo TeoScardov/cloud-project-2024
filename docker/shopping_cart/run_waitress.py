778a585975cf385f724c7181c2456060fe8394a9
TeoScardov <matt.scardovi@gmail.com>
2024-07-02 18:10:18 +0200

    updated services ports

--- docker/shopping_cart/run_waitress.py
+++ docker/shopping_cart/run_waitress.py
@@ -3,4 +3,4 @@ from app import create_app  # replace with the actual import path
 
 if __name__ == '__main__':
     app = create_app(config_class="production")
-    serve(app, host='0.0.0.0', port=4000)
+    serve(app, host='0.0.0.0', port=4005)
