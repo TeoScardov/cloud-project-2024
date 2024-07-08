98caf27e72f9299e1f67f39b4a4743b3fcca2198
zamb0 <zamboni.fabio23@gmail.com>
2024-07-08 19:00:23 +0200

    use just one db

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -15,83 +15,103 @@ resource "aws_db_subnet_group" "ebook_store_db_subnet_group" {
 ## RDS instance ##
 ##################
 
-resource "aws_db_instance" "account_db" {
-  identifier           = "accountdb"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "11.5"
-  instance_class       = "db.t2.micro"
-  username             = var.postgres_account_username
-  password             = var.postgres_account_password
-  db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  skip_final_snapshot  = true
-  availability_zone = var.db_availability_zone
-  multi_az = var.enable_multi_az
+resource "aws_rds_cluster" "ebook_store_db" {
+  cluster_identifier      = "db"
+  engine                  = "aurora-postgresql"
+  engine_version          = "15.1"
+  database_name           = var.postgres_db_name
+  master_username         = var.postgres_username
+  master_password         = var.postgres_password
+  db_subnet_group_name    = aws_db_subnet_group.ebook_store_db_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.db_sg.id]
+  availability_zones      = var.db_availability_zone
+  skip_final_snapshot     = true
+  backup_retention_period = 7
+  apply_immediately       = true
   tags = {
-    Name = "account_db"
+    Name = "ebook_store_db"
     Terraform = "true"
   }
+  
 }
 
-resource "aws_db_instance" "product_db" {
-  identifier           = "productdb"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "11.5"
-  instance_class       = "db.t2.micro"
-  username             = var.postgres_product_username
-  password             = var.postgres_product_password
-  db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  skip_final_snapshot  = true
-  availability_zone = var.db_availability_zone
-  multi_az = var.enable_multi_az
-  tags = {
-    Name = "product_db"
-    Terraform = "true"
-  }
-}
+# resource "aws_db_instance" "account_db" {
+#   identifier           = "accountdb"
+#   allocated_storage    = 20
+#   storage_type         = "gp2"
+#   engine               = "postgres"
+#   engine_version       = "11.5"
+#   instance_class       = "db.t2.micro"
+#   username             = var.postgres_account_username
+#   password             = var.postgres_account_password
+#   db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
+#   vpc_security_group_ids = [aws_security_group.db_sg.id]
+#   skip_final_snapshot  = true
+#   availability_zone = var.db_availability_zone
+#   multi_az = var.enable_multi_az
+#   tags = {
+#     Name = "account_db"
+#     Terraform = "true"
+#   }
+# }
 
-resource "aws_db_instance" "purchase_db" {
-  identifier           = "purchasedb"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "11.5"
-  instance_class       = "db.t2.micro"
-  username             = var.postgres_purchase_username
-  password             = var.postgres_purchase_password
-  db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  skip_final_snapshot  = true
-  availability_zone = var.db_availability_zone
-  multi_az = var.enable_multi_az
-  tags = {
-    Name = "purchase_db"
-    Terraform = "true"
-  }
-}
+# resource "aws_db_instance" "product_db" {
+#   identifier           = "productdb"
+#   allocated_storage    = 20
+#   storage_type         = "gp2"
+#   engine               = "postgres"
+#   engine_version       = "11.5"
+#   instance_class       = "db.t2.micro"
+#   username             = var.postgres_product_username
+#   password             = var.postgres_product_password
+#   db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
+#   vpc_security_group_ids = [aws_security_group.db_sg.id]
+#   skip_final_snapshot  = true
+#   availability_zone = var.db_availability_zone
+#   multi_az = var.enable_multi_az
+#   tags = {
+#     Name = "product_db"
+#     Terraform = "true"
+#   }
+# }
+
+# resource "aws_db_instance" "purchase_db" {
+#   identifier           = "purchasedb"
+#   allocated_storage    = 20
+#   storage_type         = "gp2"
+#   engine               = "postgres"
+#   engine_version       = "11.5"
+#   instance_class       = "db.t2.micro"
+#   username             = var.postgres_purchase_username
+#   password             = var.postgres_purchase_password
+#   db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
+#   vpc_security_group_ids = [aws_security_group.db_sg.id]
+#   skip_final_snapshot  = true
+#   availability_zone = var.db_availability_zone
+#   multi_az = var.enable_multi_az
+#   tags = {
+#     Name = "purchase_db"
+#     Terraform = "true"
+#   }
+# }
 
 
-resource "aws_db_instance" "cart_db" {
-  identifier           = "cartdb"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "11.5"
-  instance_class       = "db.t2.micro"
-  username             = var.postgres_cart_username
-  password             = var.postgres_cart_password
-  db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  skip_final_snapshot  = true
-  availability_zone = var.db_availability_zone
-  multi_az = var.enable_multi_az
-  tags = {
-    Name = "cart_db"
-    Terraform = "true"
-  }
-}
+# resource "aws_db_instance" "cart_db" {
+#   identifier           = "cartdb"
+#   allocated_storage    = 20
+#   storage_type         = "gp2"
+#   engine               = "postgres"
+#   engine_version       = "11.5"
+#   instance_class       = "db.t2.micro"
+#   username             = var.postgres_cart_username
+#   password             = var.postgres_cart_password
+#   db_subnet_group_name = aws_db_subnet_group.ebook_store_db_subnet_group.name
+#   vpc_security_group_ids = [aws_security_group.db_sg.id]
+#   skip_final_snapshot  = true
+#   availability_zone = var.db_availability_zone
+#   multi_az = var.enable_multi_az
+#   tags = {
+#     Name = "cart_db"
+#     Terraform = "true"
+#   }
+# }
--- terraform/rds_variable.tf
+++ terraform/rds_variable.tf
@@ -1,46 +1,20 @@
-variable "postgres_account_password" {
+variable "postgres_password" {
   description = "The password for the account database"
   type        = string  
 }
 
-variable "postgres_account_username" {
+variable "postgres_username" {
   description = "The username for the account database"
   type        = string  
 }
 
-variable "postgres_product_password" {
-  description = "The password for the product database"
+variable "postgres_db_name" {
+  description = "The name of the database"
   type        = string  
 }
-
-variable "postgres_product_username" {
-  description = "The username for the product database"
-  type        = string  
-}
-
-variable "postgres_purchase_password" {
-  description = "The password for the purchase database"
-  type        = string  
-}
-
-variable "postgres_purchase_username" {
-  description = "The username for the purchase database"
-  type        = string  
-}
-
-variable "postgres_cart_password" {
-  description = "The password for the cart database"
-  type        = string  
-}
-
-variable "postgres_cart_username" {
-  description = "The username for the cart database"
-  type        = string  
-}
-
 variable "db_availability_zone" {
   description = "The availability zone for the database"
-  type        = string  
+  type        = list(string)
 }
 
 variable "enable_multi_az" {
