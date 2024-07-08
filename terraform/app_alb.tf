18e478aa5fc30b16f8482b8c5e6aefa0943eb5be
zamb0 <zamboni.fabio23@gmail.com>
2024-07-08 19:01:06 +0200

    fix lb arn ref

--- terraform/app_alb.tf
+++ terraform/app_alb.tf
@@ -147,7 +147,7 @@ resource "aws_lb_target_group" "cart_lb_target_group" {
 ##################
 
 resource "aws_lb_listener" "app_lb_listener" {
-  load_balancer_arn = aws_lb.application_load_balancer.arn
+  load_balancer_arn = aws_lb.app_lb.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
