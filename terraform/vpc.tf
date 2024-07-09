f001dbfd862fd390bd011a7b9bbbf31766af4a1e
zamb0 <zamboni.fabio23@gmail.com>
2024-07-09 22:13:24 +0200

    clear vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -13,28 +13,4 @@ resource "aws_vpc" "ebook_store_vpc" {
 
 }
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
-#   version = "5.8.1"
-
-#   name = "ebook_store_vpc"
-#   cidr = var.vpc_cidr
-
-#   azs             = var.azs
-#   private_subnets = var.private_subnets_cidr
-#   public_subnets  = var.public_subnets_cidr
-
-#   private_subnet_names = var.private_subnets_names
-#   public_subnet_names = var.public_subnets_names
-
-#   enable_nat_gateway = false#true
-#   enable_vpn_gateway = false#true
-
-#   tags = {
-#     Name = "ebook_store_vpc"
-#     Terraform = "true"
-#     Environment = "dev"
-#   }
-# }
-
 
