afa6e9f401c619897a15075317433a37a3e56d49
zamb0 <zamboni.fabio23@gmail.com>
2024-07-09 22:16:19 +0200

    add dependency

--- terraform/nat_gateway.tf
+++ terraform/nat_gateway.tf
@@ -4,7 +4,6 @@
 
 resource "aws_eip" "eip_nat_gateway" {
     domain = "vpc"
-
     tags = {
       name = "eip_nat_gateway"
     }
@@ -13,7 +12,10 @@ resource "aws_eip" "eip_nat_gateway" {
 
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.eip_nat_gateway.id
-  subnet_id     = aws_subnet.ebook_store_public_subnet_web_2.id
+  subnet_id     = aws_subnet.ebook_store_public_subnet_web_1.id
+
+  depends_on = [aws_internet_gateway.ebook_store_igw]
+
   tags = {
     Name = "nat_gateway"
     Terraform = "true"
