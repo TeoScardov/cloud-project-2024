ef50914220e80210c71c9080a23ee57d6ba8f8c5
zamb0 <zamboni.fabio23@gmail.com>
2024-07-09 22:17:13 +0200

    add route53 record

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -6,12 +6,23 @@ resource "aws_route53_record" "entrypoint_web_lb_record" {
   zone_id = aws_route53_zone.ebook_store_zone.zone_id
   name    = "elastic-book-store.com"
   type    = "A"
-  records = [aws_lb.web_lb.dns_name]
+  #ttl     = "300"
+  alias {
+    name    = aws_lb.web_lb.dns_name
+    zone_id = aws_lb.web_lb.zone_id
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_route53_record" "entrypoint_app_lb_record" {
   zone_id = aws_route53_zone.ebook_store_zone.zone_id
   name    = "api.elastic-book-store.com"
   type    = "A"
-  records = [aws_lb.app_lb.dns_name]
+  #ttl     = "300"
+
+  alias {
+    name    = aws_lb.app_lb.dns_name
+    zone_id = aws_lb.app_lb.zone_id
+    evaluate_target_health = true
+  }
 }
