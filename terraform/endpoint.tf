8125e1b1ce94d5d9ab44b7a1bb97b1525883bd95
zamb0 <zamboni.fabio23@gmail.com>
2024-07-11 16:28:53 +0200

    add s3 and autoscaling endpoints

--- terraform/endpoint.tf
+++ terraform/endpoint.tf
@@ -80,3 +80,48 @@ resource "aws_vpc_endpoint" "ssm-endpoint" {
       Name = "ssm-endpoint"
   }
 }
+
+resource "aws_vpc_endpoint" "s3-endpoint1" {
+  vpc_id       = aws_vpc.ebook_store_vpc.id
+  service_name = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = [aws_route_table.ebook_store_private_route_table.id]
+
+  tags = {
+      Name = "s3-endpoint1"
+  }
+} 
+resource "aws_vpc_endpoint" "s3-endpoint2" {
+  vpc_id       = aws_vpc.ebook_store_vpc.id
+  service_name = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [aws_subnet.ebook_store_private_subnet_application_1.id, aws_subnet.ebook_store_private_subnet_application_2.id]
+  security_group_ids = [aws_security_group.endpoints_sg.id]
+
+  tags = {
+      Name = "s3-endpoint2"
+  }
+} 
+resource "aws_vpc_endpoint" "s3-endpoint3" {
+  vpc_id       = aws_vpc.ebook_store_vpc.id
+  service_name = "com.amazonaws.s3-global.accesspoint"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [aws_subnet.ebook_store_private_subnet_application_1.id, aws_subnet.ebook_store_private_subnet_application_2.id]
+  security_group_ids = [aws_security_group.endpoints_sg.id]
+
+  tags = {
+      Name = "s3-endpoint3"
+  }
+} 
+
+resource "aws_vpc_endpoint" "autoscaling-endpoint" {
+  vpc_id      = aws_vpc.ebook_store_vpc.id
+  service_name = "com.amazonaws.us-east-1.autoscaling"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [aws_subnet.ebook_store_private_subnet_application_1.id, aws_subnet.ebook_store_private_subnet_application_2.id]
+  security_group_ids = [aws_security_group.endpoints_sg.id]
+
+  tags = {
+      Name = "autoscaling-endpoint"
+  } 
+}
