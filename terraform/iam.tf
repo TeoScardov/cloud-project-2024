c953808714f95f4a860e2a94733eafc871592eb5
zamb0 <zamboni.fabio23@gmail.com>
2024-07-11 16:29:48 +0200

    clear roles

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -79,6 +79,8 @@ resource "aws_iam_policy" "ecr_push_policy" {
     
 }
 
+
+
 resource "aws_iam_user_policy_attachment" "github_actions_policy_attachment" {
   user       = aws_iam_user.github_actions.name
   policy_arn = aws_iam_policy.ecr_push_policy.arn
@@ -138,10 +140,6 @@ resource "aws_iam_policy" "ecr-policy" {
                 "ecr:BatchGetImage",
                 "ecr:DescribeImages",
                 "ecr:DescribeRepositories",
-
-                "ssm:GetParameters",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
             ],
             Resource = "*"
         }
@@ -177,108 +175,14 @@ resource "aws_iam_role" "ecs_task_role" {
   })
 }
 
-# resource "aws_iam_role_policy_attachment" "ecs-cli-policy-attachment" {
-#   role       = aws_iam_role.ecs-task-role.id
-#   policy_arn = "arn:aws:iam::483451515855:policy/ECS-for-CLI-Exec"
-# }
-
 resource "aws_iam_role_policy_attachment" "ecs-task-role-AmazonECS_FullAccesst" {
   role       = aws_iam_role.ecs_task_role.id
   policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "ecs-task-role-CloudWatchFullAccess" {
+  role      = aws_iam_role.ecs_task_role.id
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
 
-# resource "aws_iam_role" "ecr_access_role" {
-#   name               = "ecr-access-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ecr.amazonaws.com"
-#         }
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-  
-#   })
-# }
 
-# resource "aws_iam_role" "ecs_task_execution_role" {
-#   name = "ecs_task_execution_role"
-#   description = "Allows the execution of ECS tasks"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ecs-tasks.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
-
-# resource "aws_iam_role" "ecs_task_role" {
-#   name = "ecs_task_role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ecs-tasks.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
-
-# #############################################
-# ## Attach policies ecr-role ##
-# #############################################
-
-# resource "aws_iam_role_policy_attachment" "ecr-role-policy-attachment-pull" {
-#    role       = aws_iam_role.ecr_access_role.id
-#    policy_arn = aws_iam_policy.ecr_pull_policy.arn
-# }
-
-# resource "aws_iam_role_policy_attachment" "ecr-role-policy-attachment-push" {
-#   role       = aws_iam_role.ecr_access_role.id
-#   policy_arn = aws_iam_policy.ecr_push_policy.arn
-  
-# }
-
-# #############################################
-# ## Attach policies ecs-task-execution-role ##
-# #############################################
-
-# resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment-ecs" {
-#   role       = aws_iam_role.ecs_task_execution_role.id
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-# }
-
-# resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment-cw" {
-#   role       = aws_iam_role.ecs_task_execution_role.id
-#   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
-# }
-
-# ###################################
-# ## Attach policies ecs-task-role ##
-# ###################################
-
-# # resource "aws_iam_role_policy_attachment" "ecs-cli-policy-attachment" {
-# #   role       = aws_iam_role.ecs_task_role.id
-# #   policy_arn = "arn:aws:iam::483451515855:policy/ECS-for-CLI-Exec"
-# # }
-
-# resource "aws_iam_role_policy_attachment" "ecs-resources-full-access-policy-attachment" {
-#   role       = aws_iam_role.ecs_task_role.id
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
-# }
