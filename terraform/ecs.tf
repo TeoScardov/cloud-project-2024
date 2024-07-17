72979ad1f2b0b441948ec1b6aac467e2912930f5
zamb0 <zamboni.fabio23@gmail.com>
2024-07-17 17:31:49 +0200

    fix runtime

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -197,6 +197,11 @@ resource "aws_ecs_task_definition" "ecs_task_definition_web" {
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn      = aws_iam_role.ecs_task_role.arn
 
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture = "X86_64"
+  }
+
   container_definitions = jsonencode([
     {
       name      = "web"
