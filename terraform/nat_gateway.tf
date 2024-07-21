e771da7538dc9f23b01004de945251a1f672dcb1
zamb0 <zamboni.fabio23@gmail.com>
2024-07-21 16:09:10 +0200

    add nat gateway

--- terraform/nat_gateway.tf
+++ terraform/nat_gateway.tf
@@ -1,23 +1,23 @@
-#################
-## NAT Gateway ##
-#################
+################
+# NAT Gateway ##
+################
 
-# resource "aws_eip" "eip_nat_gateway" {
-#     domain = "vpc"
-#     tags = {
-#       name = "eip_nat_gateway"
-#     }
+resource "aws_eip" "eip_nat_gateway" {
+    domain = "vpc"
+    tags = {
+      name = "eip_nat_gateway"
+    }
   
-# }
+}
 
-# resource "aws_nat_gateway" "nat_gateway" {
-#   allocation_id = aws_eip.eip_nat_gateway.id
-#   subnet_id     = aws_subnet.ebook_store_public_subnet_web_1.id
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eip_nat_gateway.id
+  subnet_id     = aws_subnet.ebook_store_public_subnet_web_1.id
 
-#   depends_on = [aws_internet_gateway.ebook_store_igw]
+  depends_on = [aws_internet_gateway.ebook_store_igw]
 
-#   tags = {
-#     Name = "nat_gateway"
-#     Terraform = "true"
-#   }
-# }
+  tags = {
+    Name = "nat_gateway"
+    Terraform = "true"
+  }
+}
