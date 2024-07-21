c47adc491262db3e16ac4b5ae5479892281dfb6c
zamb0 <zamboni.fabio23@gmail.com>
2024-07-21 16:55:38 +0200

    fix api urls

--- docker/shopping_cart/cartApp/config.py
+++ docker/shopping_cart/cartApp/config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-PRODUCT_SERVICE_URL = environ.get("PRODUCT_CATALOG_URL", 'http://product_catalog:4003/api/product')
-USER_SERVICE_URL = environ.get("USER_SERVICE_URL", 'http://account_management:4001/api/account')
+PRODUCT_SERVICE_URL = environ.get("PRODUCT_CATALOG_URL", 'http://product_catalog:4003/api/product') 
+USER_SERVICE_URL = environ.get("USER_SERVICE_URL", 'http://account_management:4001/api/account') 
 class Config(object):
     """Base config."""
     DEBUG = False
@@ -33,19 +33,3 @@ def get_config_map(name):
 
     return config_map[name]
 
-# """Base config."""
-# JWT_SECRET_KEY = 'nyquist'
-# PRODUCT_SERVICE_URL = 'http://product_catalog:4000/api/product'
-# # PRODUCT_SERVICE_URL = 'http://127.0.0.1:5000/api/product'
-# USER_SERVICE_URL = 'http://account_management:4000/api/account'
-# # USER_SERVICE_URL = 'http://127.0.0.1:5001/api/account'
-# SWAGGER = {
-#     'title': 'Cart Service API',
-#     'uiversion': 3,
-#     'specs_route': '/api/docs/',
-#     'version': '1.0',
-#     'openapi': '3.0.2',
-#     'description': 'API for Cart Service',
-# }
-# #TESTING = False
-# TESTING = True
